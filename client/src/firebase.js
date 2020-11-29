7e82eb491074e046a76a87a58064fa65055e288e
Peter Sammon <petersammon2@gmail.com>
2020-11-29 14:43:22 -0500

    firebase setup & spotify clientID

---
+++ client/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyBy_f2iONn7LoNcg3Wte7vLFyST6kZKE_E',
+	authDomain: 'spotify-react-cf8fa.firebaseapp.com',
+	databaseURL: 'https://spotify-react-cf8fa.firebaseio.com',
+	projectId: 'spotify-react-cf8fa',
+	storageBucket: 'spotify-react-cf8fa.appspot.com',
+	messagingSenderId: '921123748323',
+	appId: '1:921123748323:web:a7945e0f649be86514c8d6',
+	measurementId: 'G-GQB5QCNHQ1',
+};
