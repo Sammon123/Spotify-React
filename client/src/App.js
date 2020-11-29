f9e12a7dcd903bc61b1af5d8b1374cbfe8a1f499
Peter Sammon <petersammon2@gmail.com>
2020-11-29 15:33:12 -0500

    set token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+import { getTokenFromUrl } from './spotify';
+
+const spotify = new SpotifyWebApi();
+
 function App() {
+	useEffect(() => {
+		const hash = getTokenFromUrl();
+		window.location.hash = '';
+		let _token = hash.access_token;
+		if (_token) {
+			spotify.setAccessToken(_token);
+		}
+	}, []);
 	return (
 		<div className='App'>
 			<h1>Spotify-React</h1>
