2fdbcd54f205e5340a71e6029333b9c2b5195196
Peter Sammon <petersammon2@gmail.com>
2020-11-29 20:04:14 -0500

    added sidebar content

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
+import SidebarOption from '../SidebarOption/SidebarOption';
+import { useStateValue } from '../../StateProvider';
 
 const Sidebar = () => {
+	const [{ playlists }] = useStateValue();
 	return (
-		<div>
-			<h1>Sidebar</h1>
+		<div className='sidebar'>
+			<img
+				src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg'
+				alt=''
+			/>
+			<SidebarOption Icon={HomeIcon} title='Home' />
+			<SidebarOption Icon={SearchIcon} title='Search' />
+			<SidebarOption Icon={LibraryMusicIcon} title='YourLibrary' />
+			<br />
+			<strong className='sidebar-title'>PLAYLISTS</strong>
+			<hr />
+			{playlists?.items?.map((playlist) => (
+				<SidebarOption title={playlist.name} />
+			))}
 		</div>
 	);
 };
