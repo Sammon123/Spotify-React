7f20327d531bdc638b378b46885fd40712d30050
Peter Sammon <petersammon2@gmail.com>
2020-11-29 20:21:10 -0500

    styling sidebar, bug fixes

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,7 +1,7 @@
 export const initialState = {
 	user: null,
-	token:
-		'BQDWXm8n19RpU72JNUoxgSCdQO02Vy2sesPimCwpjpCDLVHeDhhtPhthLm2WuuG_LviIleiGilOGDXCnu77XlbFCf6fMZ0YRMtB9-cd6_hvSNi-6SiubtkBdFW61rxAFxuZx5saSdFxEVykmvcfJLXWAQvJhtd7K',
+	token: null,
+	// 'BQDWXm8n19RpU72JNUoxgSCdQO02Vy2sesPimCwpjpCDLVHeDhhtPhthLm2WuuG_LviIleiGilOGDXCnu77XlbFCf6fMZ0YRMtB9-cd6_hvSNi-6SiubtkBdFW61rxAFxuZx5saSdFxEVykmvcfJLXWAQvJhtd7K',
 	playlists: [],
 };
 
