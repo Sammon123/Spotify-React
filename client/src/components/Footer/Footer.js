5d7e643182b696e3b9c6b6362da8d419c0307e43
Peter Sammon <petersammon2@gmail.com>
2021-01-20 13:38:50 -0500

    giving footer initial state

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useStateValue } from '../../StateProvider';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
@@ -11,15 +11,45 @@ import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
 import { Grid, Slider } from '@material-ui/core';
 import './Footer.css';
 
-const Footer = () => {
+const Footer = ({ spotify }) => {
 	const [{ item, playing }, dispatch] = useStateValue();
+
+	useEffect(() => {
+		spotify.getMyCurrentPlaybackState().then((res) => {
+			console.log(res);
+
+			dispatch({
+				type: 'SET_PLAYING',
+				playing: res.is_playing,
+			});
+
+			dispatch({
+				type: 'SET_ITEM',
+				item: res.item,
+			});
+		});
+	}, [spotify, dispatch]);
 	return (
 		<div className='footer'>
 			<div className='footer-left'>
-				<img className='footer-album' src='' alt='' />
-				<div className='footer-songInfo'>
-					<h4>No song is playing</h4>
-				</div>
+				<img
+					className='footer-album'
+					src={item?.item?.album?.images[0]?.url || item?.album?.images[0]?.url}
+					alt={item?.name}
+				/>
+				{item ? (
+					<div className='footer-songInfo'>
+						<h4>{item?.item?.name || item?.name}</h4>
+						<p>
+							{item?.artists?.map((artist) => artist?.name).join(', ') ||
+								item?.item?.artists.map((artist) => artist?.name).join(', ')}
+						</p>
+					</div>
+				) : (
+					<div className='footer-songInfo'>
+						<h4>No song is playing</h4>
+					</div>
+				)}
 			</div>
 			<div className='footer-center'>
 				<ShuffleIcon className='footer-green' />
