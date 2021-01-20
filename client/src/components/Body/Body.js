2ff48fd5783f32f2711f01f6e12cebf37caaadc4
Peter Sammon <petersammon2@gmail.com>
2021-01-20 13:52:55 -0500

    content for body and the header

--- client/src/components/Body/Body.js
+++ client/src/components/Body/Body.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import './Body.css';
+import { useStateValue } from '../../StateProvider';
+import Header from '../Header/Header';
 
-const Body = () => {
+const Body = ({ spotify }) => {
+	const [{ discover_weekly }, dispatch] = useStateValue();
 	return (
-		<div>
-			<h1>Body</h1>
+		<div className='body'>
+			<Header spotify={spotify} />
+			<div className='body-info'>
+				<img src='' alt='' />
+				<div className='body-infoText'>
+					<strong>PLAYLIST</strong>
+					<h2>Discover Weekly</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+						consequuntur dolores libero, reiciendis eveniet explicabo fuga alias
+						voluptate odio quibusdam animi, odit amet minus iure perferendis,
+						enim laborum facere reprehenderit.
+					</p>
+				</div>
+			</div>
+			<div className='body-songs'>
+				<div className='body-icon'>
+					<PlayCircleFilledIcon className='body-shuffle' />
+					<FavoriteIcon fontSize='large' />
+					<MoreHorizIcon />
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Avatar } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import { useStateValue } from '../../StateProvider';
+
+const Header = () => {
+	const [{ user }] = useStateValue();
+	return (
+		<div className='header'>
+			<div className='header-left'>
+				<SearchIcon />
+				<input type='text' placeholder='Search for Artists, Songs or Albums' />
+			</div>
+			<div className='header-right'>
+				<Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+				<h4>{user?.display_name}</h4>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
